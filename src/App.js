9ee413422c63b23c4882f34dba2ce238fe5df44a
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-06-30 12:26:50 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "4136e6df";
+  const APP_KEY = "0697c6c9b15b7f49e289f9e5c93f42fb";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header"></header>
-      hello
+      <form className="search-form">
+        <input className="search-bar" type="text">
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </input>
+      </form>
     </div>
   );
-}
-
+};
 export default App;
